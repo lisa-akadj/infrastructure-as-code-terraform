39b7a2cf5613ab591bcd48f1a4abca90a95f6bd7
Dunjinmaa Jamsran <dunjinmaa@gmail.com>
2026-04-15 13:50:41 +0100

    terraform yml file created with Gemini

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+terraform {
+    backend "s3" {
+        bucket = "dunjee-terraform-bucket"
+        key = "terraform.tfstate"
+        region = "eu-west-2"
+    }
+}
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,4 @@
 terraform {
-    backend "s3" {
-        bucket = "dunjee-terraform-bucket"
-        key = "path/to/my/key"
-        region = "eu-west-2"
-    }
-
     required_providers {
         aws = {
             source = "hashicorp/aws"
